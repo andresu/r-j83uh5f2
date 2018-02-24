8e24ac0aa36480ab3da8f9b60ea1f277367bbb03
Andres Urrea <andres.urreao@gmail.com>
2018-02-24 17:07:26 -0500

    Counter-functionadity

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            text: ""
+        };
+        this.handleCount = this.handleCount.bind(this); 
+    }
+    getInitialState(){
+        return{
+            text: this.props.text,
+        }
+    }
+
+    handleCount(ev){
+        this.setState({
+           text:ev.target.value
+        });
+        
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <textarea rows="3" onChange={this.handleCount} value={this.state.text}></textarea>
+                <div className="counter">{this.state.text.length}</div>
+            </div>
+            );
+    }
 }
 
 export default App;
